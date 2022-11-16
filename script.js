3f419e8850daabc4a0a32c288370f03be596791b
Razvan Sauciuc <117271210+RazvanSauciuc@users.noreply.github.com>
2022-11-16 14:19:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let raw = [];
-let i = 0;
+let rawSearch = [];
+let i = 0, j = 0;
 
 function addWords() {
     raw[i] = document.getElementById("text").value;
-    document.getElementById('textinput').innerHTML = "The word " + raw[i] + " has been added";
-    for (let j = 0; j < i; ++j) {
-        if (raw[i] === raw[j]) {
-            document.getElementById('textinput').innerHTML = 'This word it is already in dictionary';
-        }
-    }
+    document.getElementById('textinput').innerHTML = "The word " + raw[i] + " has been added!";
     ++i;
 }
 
+function searchWords() {
+    rawSearch[j] = document.getElementById("text").value;
+    for (let x = 0; x < i; ++x) {
+        if (raw[x] === rawSearch[j]) {
+             document.getElementById('textinput').innerHTML = 'This word it is already in dictionary!';
+        } else {
+             document.getElementById('textinput').innerHTML = 'This word was not found in dictionary!';
+        }
+    }
+}
